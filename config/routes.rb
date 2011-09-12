7c6af64d0c4db5de1f98d5c8c6a1406910343b7a
Mark Linn <marklinn@xwcsolutions.com>
2011-09-11 18:09:54 -0700

    Setup appropriate routing for not just all videos but videos for products and removed improper methods on non-admin controllers that default scaffolding created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Add your extension routes here
   resources :videos
+  resources :products do
+    match 'videos' => 'videos#product_index'
+  end
   namespace :admin do
     resources :products do
       resources :videos do
