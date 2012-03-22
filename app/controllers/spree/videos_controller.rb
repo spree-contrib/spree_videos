73a832e9fbbe739907c2ce7f890db0048f9eb7b8
Aleksey <aleksey.dem@gmail.com>
2012-03-22 19:22:46 +0400

    Table name fix. spree_products instead products

--- app/controllers/spree/videos_controller.rb
+++ app/controllers/spree/videos_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   class VideosController < BaseController
     def index
-      @videos = Video.all(:joins => :product, :conditions => 'products.deleted_at is NULL')
+      @videos = Video.all(:joins => :product, :conditions => 'spree_products.deleted_at is NULL')
 
       respond_to do |format|
         format.html # index.html.erb
@@ -20,11 +20,11 @@ module Spree
     def show
       video = Video.find(params[:id])
       client = YouTubeIt::Client.new
-    
+
       # not sure why the original dev required the configuration options
       # maybe youtube_it was updated?
       # client = YouTubeIt::Client.new(:dev_key => YouTubeITConfig.dev_key)
-    
+
       @video = client.video_by(video.youtube_ref)
       respond_to do |format|
         format.html # show.html.erb
