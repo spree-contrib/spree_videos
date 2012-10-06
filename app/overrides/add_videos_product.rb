793e14a624a6c1df39a14fc83e89312d8466fa07
Michael Bianco <iloveitaly@gmail.com>
2012-10-06 16:44:30 -0400

    Another typo

--- app/overrides/add_videos_product.rb
+++ app/overrides/add_videos_product.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'add_videos_to_products_show',
                      :insert_after => "[data-hook='product_properties']",
                      :text => %q{ <%= render :partial => 'spree/products/video', :locals => { :product => @product } %> },
-                     :disables => false)
+                     :disabled => false)
